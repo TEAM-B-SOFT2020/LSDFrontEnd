dc7a765ae7c4cb68c678c9b2137df43946e8d1b6
Ddomino <Andreasj54@gmail.com>
2020-11-25 16:32:48 +0100

    Set up a few tests

--- src/test/carrier.test.ts
+++ src/test/carrier.test.ts
@@ -1,5 +1,12 @@
 // api reference:: https://jestjs.io/docs/en/api
 
+import IContract from "contract";
+import Contract from "../contract/ContractMock";
+import ICarrierDetail from 'contract/src/DTO/ICarrierDetail';
+import ContractMock from "../contract/ContractMock";
+
+const contract: IContract = new Contract();
+
 // this is a custom assume test, it only runs when the condition is true
 
 // example:: database population
@@ -8,14 +15,20 @@ beforeEach(() => console.log('Running before each'));
 afterEach(() => console.log('Running after each'));
 
 // example:: database connection or setup
-beforeAll(() => console.log('Running before all'));
+beforeAll(() => {	
+		
+	
+	});
 // example:: database disconnection or cleanup
 afterAll(() => console.log('Running after all'));
 
 // creates a block that groups together several related tests
 describe('Example Test for Github Actions', () => {
 	// runs the test
-	test('True should be truthy', () => {
-		expect(true).toBeTruthy();
+	test('True should be truthy', async () => {
+		let result = await (await contract.getCarrierInformation('SAS140'));
+		const expected: ICarrierDetail= {iata: 'SAS140', name: 'SAS'};		
+		expect(expected).toEqual(result);
 	});
 });
+
