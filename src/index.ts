ade06b48b9f9b21f3f6c930e9125993a658cfb76
Srax <thomas_ebsen@live.dk>
2020-11-29 15:16:56 +0100

    .
    .

--- src/index.ts
+++ src/index.ts
@@ -3,42 +3,7 @@ import express from 'express/';
 import path from 'path';
 import dotenv from 'dotenv';
 
-
-const http = require('http')
-const url = require('url')
-const client = require('prom-client')
-// Create a Registry which registers the metrics
-const register = new client.Registry()
-// Add a default label which is added to all metrics
-register.setDefaultLabels({
-  app: 'example-nodejs-app'
-})
-// Enable the collection of default metrics
-client.collectDefaultMetrics({ register })
-// Create a histogram metric
-const httpRequestDurationMicroseconds = new client.Histogram({
-  name: 'http_request_duration_seconds',
-  help: 'Duration of HTTP requests in microseconds',
-  labelNames: ['method', 'route', 'code'],
-  buckets: [0.1, 0.3, 0.5, 0.7, 1, 3, 5, 7, 10]
-})
-// Register the histogram
-register.registerMetric(httpRequestDurationMicroseconds)
-// Define the HTTP server
-const server = http.createServer(async (req: any, res: any) => {
-    // Start the timer
-  const end = httpRequestDurationMicroseconds.startTimer()
-  // Retrieve route from request object
-  const route = url.parse(req.url).pathname
-  if (route === '/metrics') {
-    // Return all metrics the Prometheus exposition format
-    res.setHeader('Content-Type', register.contentType)
-    res.end(register.metrics())
-  }
-  // End timer and add labels
-  end({ route, code: res.statusCode, method: req.method })
-})
-
+const promMid = require('express-prometheus-middleware');
 
 dotenv.config();
 
@@ -56,7 +21,23 @@ import airports from './routes/pages/airports';
 const port: any = process.env.PORT;
 const app: express.Application = express();
 
-
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true,
+  // How often prometheus should collect the metrics
+  interval: 60 * 1000,  
+  // Any routes that should be ignored
+  excludeRoutes: [],  
+  // Percentiles for request duration summary
+  requestDurationBuckets: [0.5, 0.9, 0.95, 0.99],  
+  // Time buckets for request duration histogram
+  requestDurationHistogramBuckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10],  
+  // Size buckets for request
+  requestSizeBuckets: [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],  
+  // Size buckets for response
+  responseSizeBuckets: [5, 10, 25, 50, 100, 250, 500, 1000, 2500, 5000, 10000],
+ }));
+ 
 
 // ejs configuration
 app.set('views', path.join(__dirname, 'views'));
@@ -81,5 +62,3 @@ app.listen(port, () => {
 	console.log('Server running...');
 	console.table({ 'EJS Views': `http://localhost:${port}`, 'REST API': `http://localhost:${port}/api/` });
 });
-
-server.listen(4000)
