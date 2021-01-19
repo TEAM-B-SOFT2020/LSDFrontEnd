801a2dbf7c20ba7d05197226ba22ce86110dfc0f
Srax <thomas_ebsen@live.dk>
2021-01-19 11:29:07 +0100

    Update booking.test.ts

--- src/test/booking.test.ts
+++ src/test/booking.test.ts
@@ -2,14 +2,12 @@
 
 import IContract from "contract";
 import Contract from "../contract";
-import IAirportDetail from "contract/src/DTO/IAirportDetail";
 import IPassengerIdentifier from "contract/src/IPassengerIdentifier";
-import IBookingDetail from "contract/src/DTO/IBookingDetail";
 const contract: IContract = new Contract();
 // creates a block that groups together several related tests
-describe('Success Secnario', () => {
+describe('Success Scenario', () => {
 	// runs the test
-	test('Booking', async () => {
+	test('Get booking in database by PNR', async () => {
 		const pnr: string = "B1BS34"
 		const passengerIdentifier: IPassengerIdentifier = { pnr }
 		const expectedPrice: number = 1020;
